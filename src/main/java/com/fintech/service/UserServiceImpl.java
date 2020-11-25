19b2ac8504ed5695bacffa7724d13d1ee0025cff
initializrc <panji.cahyountoro@gmail.com>
2020-11-25 22:18:53 +0700

    update user service implementation

--- src/main/java/com/fintech/service/UserServiceImpl.java
+++ src/main/java/com/fintech/service/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.fintech.service;
 
+
 import com.fintech.entity.User;
 import com.fintech.model.UserModel;
+import com.fintech.model.UserRequestModel;
 import com.fintech.repository.UserRepository;
 
 
@@ -14,69 +16,39 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.HttpServerErrorException;
 
 
-import java.util.List;
-
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
 
-
-    @Override
-    public UserModel saveOrUpdate(UserModel entity) { return null; }
-
     @Override
     @Transactional(readOnly = false, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
-    public UserModel register(UserModel entity) {
-        User userByUsername = userRepository.findByUsername(entity.getUsername());
+    public UserModel register(UserRequestModel requestModel) {
+        User userByUsername = userRepository.findByUsername(requestModel.getUsername());
         if (userByUsername != null && userByUsername.getId() != null)
             throw new HttpServerErrorException(HttpStatus.BAD_REQUEST, "Username already exists");
-        User userByEmail= userRepository.findByEmail(entity.getEmail());
+        User userByEmail= userRepository.findByEmail(requestModel.getEmail());
         if (userByEmail != null && userByEmail.getId() != null)
             throw new HttpServerErrorException(HttpStatus.BAD_REQUEST, "Email already exists");
-        User userByPhoneNumber = userRepository.findByPhoneNumber(entity.getPhoneNumber());
+        User userByPhoneNumber = userRepository.findByPhoneNumber(requestModel.getPhoneNumber());
         if (userByPhoneNumber != null && userByPhoneNumber.getId() !=null)
-            throw new HttpServerErrorException(HttpStatus.BAD_REQUEST, "Phonenumber already exists");
+            throw new HttpServerErrorException(HttpStatus.BAD_REQUEST, "Phone number already exists");
 
-        entity.setType(entity.getType().toUpperCase());
 
         User user = new User();
-        user.setEmail(entity.getEmail());
-        user.setUsername(entity.getUsername());
-        user.setFullName(entity.getFullName());
-        user.setPhoneNumber(entity.getPhoneNumber());
-        user.setPassword(entity.getPassword());
-        user.setType(User.Type.valueOf(entity.getType()));
+        user.setEmail(requestModel.getEmail());
+        user.setPassword(requestModel.getPassword());
+        user.setUsername(requestModel.getUsername());
+        user.setFullName(requestModel.getFullName());
+        user.setPhoneNumber(requestModel.getPhoneNumber());
+        user.setType(User.Type.valueOf(requestModel.getType().toUpperCase()));
         user = userRepository.save(user);
 
-
-        BeanUtils.copyProperties(user, entity);
-        return entity;
+        UserModel userModel = new UserModel();
+        userModel.setType(requestModel.getType().toUpperCase());
+        BeanUtils.copyProperties(user, userModel);
+        return userModel;
     }
 
-    @Override
-    public UserModel delete(UserModel entity) {
-        return null;
-    }
-
-    @Override
-    public UserModel deleteById(Integer integer) {
-        return null;
-    }
-
-    @Override
-    public UserModel findById(Integer integer) {
-        return null;
-    }
-
-    @Override
-    public List<UserModel> findAll() {
-        return null;
-    }
-
-    @Override
-    public Long countAll() {
-        return null;
-    }
 }
